640cffe30f42906236e558542277de123e48d5e5
Hosni Colina <corobonhosni@gmail.com>
2017-07-19 18:58:50 -0430

    se agrego las funciones de los gestos con hammer js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var hamburgerButton = document.getElementById('hamburger-button');
 var menuContainerJs = document.getElementById('menu-container-js');
+var bodyPage = document.body;
 
 hamburgerButton.addEventListener('touchstart',toggleMenu);
       
@@ -7,3 +8,19 @@ function toggleMenu(){
 hamburgerButton.classList.toggle('is-active')
 menuContainerJs.classList.toggle('active')
 };
+
+function desplegar(){
+  menuContainerJs.classList.add('active')
+  hamburgerButton.classList.add('is-active')
+}
+function ocultar(){
+  menuContainerJs.classList.remove('active')
+  hamburgerButton.classList.remove('is-active')
+}
+//gestos touch
+
+var gestos = new Hammer(bodyPage);
+gestos.on('swipeleft', ocultar);
+
+var gestos = new Hammer(bodyPage);
+gestos.on('swiperight', desplegar);
