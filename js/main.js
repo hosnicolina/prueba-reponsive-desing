c2145f50ec9f39bf660af51204bd781018fa5395
Hosni Colina <corobonhosni@gmail.com>
2017-07-28 01:11:28 -0430

    Se agrego los media query al javascript

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
-var hamburgerButton = document.getElementById('hamburger-button');
-var menuContainerJs = document.getElementById('menu-container-js');
-var bodyPage = document.body;
+if (window.matchMedia('(max-width:36.25em)').matches){
+  var hamburgerButton = document.getElementById('hamburger-button');
+  var menuContainerJs = document.getElementById('menu-container-js');
+  var bodyPage = document.body;
 
-hamburgerButton.addEventListener('touchstart',toggleMenu);
-      
-function toggleMenu(){
-hamburgerButton.classList.toggle('is-active')
-menuContainerJs.classList.toggle('active')
-};
+        
+  function toggleMenu(){
+  hamburgerButton.classList.toggle('is-active')
+  menuContainerJs.classList.toggle('active')
+  };
 
-function desplegar(){
-  menuContainerJs.classList.add('active')
-  hamburgerButton.classList.add('is-active')
+  function desplegar(){
+    menuContainerJs.classList.add('active')
+    hamburgerButton.classList.add('is-active')
+  }
+  function ocultar(){
+    menuContainerJs.classList.remove('active')
+    hamburgerButton.classList.remove('is-active')
+  }
+  //gestos touch
+
+
+  
+
+  var gestos = new Hammer(bodyPage);
+  gestos.on('swipeleft', ocultar);
+
+  
+  gestos.on('swiperight', desplegar);
+
+  hamburgerButton.addEventListener('touchstart',toggleMenu);
+
+  }else{
+  console.log('habla claro perro');
 }
-function ocultar(){
-  menuContainerJs.classList.remove('active')
-  hamburgerButton.classList.remove('is-active')
-}
-//gestos touch
 
-var gestos = new Hammer(bodyPage);
-gestos.on('swipeleft', ocultar);
-
-var gestos = new Hammer(bodyPage);
-gestos.on('swiperight', desplegar);
